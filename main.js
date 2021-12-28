05bbb9895164f8f018c8a1adb105cbb03acf6295
LukeZekes <41444778+LukeZekes@users.noreply.github.com>
2021-12-28 12:42:39 +0000

    L10

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            if(id >= 0)
+                this.cart.push(id)
+            else
+                this.cart.splice(this.cart.indexOf(id), 1)
+        }
+    }
 })
